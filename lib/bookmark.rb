29bc56b4aafc090e6b660c6eaede8ca66d8ba33c
Adam Harbisher <aharbisher@gmail.com>
2021-11-08 17:44:21 +0000

    added bg to bundle, used to grab table from DB'

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,4 +1,12 @@
+require 'pg'
+
 class Bookmark
+
+  conn = PG.connect( dbname: 'bookmark_manager' )
+  rs = conn.exec( "SELECT * FROM bookmarks" )
+  rs.each do |row|
+    puts "%s %s" % [ row['id'], row['url'] ]
+  end
 
   def self.all
     ["https://www.google.co.uk", "https://www.bbc.co.uk", "https://www.facebook.co.uk"]
